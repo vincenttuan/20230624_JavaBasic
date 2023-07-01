0dae33d7b1560174e012b9168bc8eb733d96644c
VincentTuan <vincentjava@yahoo.com.tw>
2023-07-01 12:22:10 +0800

    Add files via upload

--- javacode/day2/Test5.java
+++ javacode/day2/Test5.java
@@ -14,6 +14,13 @@ public class Test5 {
 		int z = 0x3A; // 0x開頭表示十六進位
 		System.out.println(z);	
 
+		int a = 5 & 3;
+		int b = 5 | 3;
+		int c = 5 ^ 3;
+		System.out.printf("5 & 3 = %d\n", a);
+		System.out.printf("5 | 3 = %d\n", b);
+		System.out.printf("5 ^ 3 = %d\n", c);
+
 	}
 
 }
