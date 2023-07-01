bbb1723b48839f93bc9857204d3fe7ff407eaeb8
VincentTuan <vincentjava@yahoo.com.tw>
2023-07-01 11:13:34 +0800

    Add files via upload

---
+++ javacode/day2/Test5.java
@@ -0,0 +1,19 @@
+package day2;
+
+public class Test5 {
+
+	public static void main(String[] args) {
+		
+		//int x = 0b00011100; // 0b 表示二進位
+		int x = 0b11100; // 0b 表示二進位
+		System.out.println(x);
+
+		int y = 017; // 0開頭表示八進位
+		System.out.println(y);	
+		
+		int z = 0x3A; // 0x開頭表示十六進位
+		System.out.println(z);	
+
+	}
+
+}
