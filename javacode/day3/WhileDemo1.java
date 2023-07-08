0eaef8519f26ae0dc90fd72ef14045cd402322b4
VincentTuan <vincentjava@yahoo.com.tw>
2023-07-08 09:56:09 +0800

    Add files via upload

--- javacode/day3/WhileDemo1.java
+++ javacode/day3/WhileDemo1.java
@@ -5,15 +5,17 @@ public class WhileDemo1 {
 
 	public static void main(String[] args) {
 		// 建立一個可以產生隨機數的物件
-		Random random = new Random();		
-		while(true) {
+		Random random = new Random();
+		boolean running = true; // 控制迴圈的變數
+		while(running) {
 			int n = random.nextInt(100); // 100 表示會動產生 0~99 的隨機數
 			if(n % 11 != 0) {
 				continue; // 提早執行下一次的迴圈
 			}
 			System.out.println(n);
 			if(n == 77) {
-				break; // 跳離迴圈
+				//break; // 跳離迴圈
+				running = false;
 			}
 		}
 
